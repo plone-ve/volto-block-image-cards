303805e2cada833071632b170950d2f9fee4ec71
David Ichim <ichim.david@gmail.com>
2021-01-22 19:01:28 +0200

    Refs #126375 added volto-block-image-cards block:
    - this will be used by the theme to display slick carousels

---
+++ src/ImageCards/ImageCardsEdit.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { SidebarPortal } from '@plone/volto/components'; // EditBlock
+import InlineForm from '@plone/volto/components/manage/Form/InlineForm';
+
+import ImageCardsView from './ImageCardsView';
+
+import schema from './schema';
+
+const ImageCardEdit = (props) => {
+  return (
+    <>
+      <ImageCardsView data={props.data} />
+
+      <SidebarPortal selected={props.selected}>
+        <InlineForm
+          schema={schema}
+          title={schema.title}
+          onChangeField={(id, value) => {
+            props.onChangeBlock(props.block, {
+              ...props.data,
+              [id]: value,
+            });
+          }}
+          formData={props.data}
+          block={props.block}
+        />
+      </SidebarPortal>
+    </>
+  );
+};
+
+export default ImageCardEdit;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const applyConfig = (config) => {
+import codeSVG from '@plone/volto/icons/code.svg';
+import { ImageCardsView, ImageCardsEdit } from './ImageCards';
+
+export default (config) => {
+  config.blocks.blocksConfig.imagecards = {
+    id: 'imagecards',
+    title: 'Image Cards',
+    icon: codeSVG,
+    group: 'custom_addons',
+    view: ImageCardsView,
+    edit: ImageCardsEdit,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: 1,
+    security: {
+      addPermission: [],
+      view: [],
+    },
+  };
   return config;
 };
-
-export default applyConfig;
