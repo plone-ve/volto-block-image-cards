03a43ff671ed3af590e6b6bc2a23c82b14c8471c
Daniela Mormocea <daniela.mormocea@gmail.com>
2021-12-20 23:19:34 +0200

    add tests

--- cypress/integration/block-basics.js
+++ cypress/integration/block-basics.js
@@ -1,5 +1,5 @@
 import { setupBeforeEach, tearDownAfterEach } from '../support';
-
+import 'cypress-file-upload';
 describe('Blocks Tests', () => {
   beforeEach(setupBeforeEach);
   afterEach(tearDownAfterEach);
@@ -18,9 +18,74 @@ describe('Blocks Tests', () => {
     cy.get('[contenteditable=true]').first().type('{enter}');
 
     // Add block
+
+
+    cy.get('[contenteditable=true]').first().type('{enter}');
     cy.get('.ui.basic.icon.button.block-add-button').first().click();
-    cy.get('.blocks-chooser .title').contains('Media').click();
-    cy.get('.content.active.media .button.image').contains('Image').click();
+    cy.get('.blocks-chooser .title').contains('Common').click();
+    cy.get('.content.active .button.imagecards').contains('Image Cards').click();
+
+    //add block image cards
+    cy.get('.field-wrapper-title #field-title').last().type('Test');
+    cy.get('.field-wrapper-text .slate-editor').type('Test text');
+    cy.get('.field-wrapper-display #field-display').click();
+    cy.get('.react-select__menu').contains('Splashy Carousel').click();
+    cy.get('.field-wrapper-align .buttons').eq(2).click();
+    cy.get('.field-wrapper-cards button').click();
+
+    //upload 1st image
+    const imageFile = 'cat.jpg';
+    cy.get('.field-wrapper-title-0-cards-0 #field-title-0-cards-0').type('Image1');
+    cy.get('.field-wrapper-text-1-cards-0 .slate-editor').type('Image1 text');
+    cy.get('.field-wrapper-attachedimage-2-cards-0 .item').attachFile(imageFile);
+
+    //add new image
+    cy.get('.field-wrapper-cards button').click();
+
+
+    //upload second image
+    cy.get('.field-wrapper-title-0-cards-1 #field-title-0-cards-1').type('Image2');
+    cy.get('.field-wrapper-text-1-cards-1 .slate-editor').type('Image1 text');
+    cy.get('.field-wrapper-attachedimage-2-cards-1 .item').attachFile(imageFile);
+
+
+    //add new block
+    cy.get('.block-editor-text [contenteditable=true]').first().click({force: true}).type('{enter}');
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get('.blocks-chooser .title').contains('Common').click();
+    cy.get('.content.active .button.imagecards').contains('Image Cards').click();
+
+    cy.get('.field-wrapper-title #field-title').last().type('Test');
+    cy.get('.field-wrapper-text .slate-editor').type('Test text');
+    cy.get('.field-wrapper-display #field-display').click();
+    cy.get('.react-select__menu').contains('Discreet Carousel').click();
+    cy.get('.field-wrapper-align .buttons').eq(1).click();
+    cy.get('.field-wrapper-cards button').click();
+
+    //upload 1st image
+    cy.get('.field-wrapper-title-0-cards-0 #field-title-0-cards-0').type('Image1');
+    cy.get('.field-wrapper-text-1-cards-0 .slate-editor').type('Image1 text');
+    cy.get('.field-wrapper-attachedimage-2-cards-0 .item').attachFile(imageFile);
+
+
+    //add new block
+    cy.get('.block-editor-text [contenteditable=true]').first().click({force: true}).type('{enter}');
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get('.blocks-chooser .title').contains('Common').click();
+    cy.get('.content.active .button.imagecards').contains('Image Cards').click();
+
+    cy.get('.field-wrapper-title #field-title').last().type('Test');
+    cy.get('.field-wrapper-text .slate-editor').type('Test text');
+    cy.get('.field-wrapper-display #field-display').click();
+    cy.get('.react-select__menu').contains('Round Tile').click();
+    cy.get('.field-wrapper-align .buttons').eq(0).click();
+    cy.get('.field-wrapper-cards button').click();
+
+    //upload 1st image
+    cy.get('.field-wrapper-title-0-cards-0 #field-title-0-cards-0').type('Image1');
+    cy.get('.field-wrapper-text-1-cards-0 .slate-editor').type('Image1 text');
+    cy.get('.field-wrapper-attachedimage-2-cards-0 .item').attachFile(imageFile);
+
 
     // Save
     cy.get('#toolbar-save').click();
@@ -28,6 +93,8 @@ describe('Blocks Tests', () => {
 
     // then the page view should contain our changes
     cy.contains('My Add-on Page');
-    cy.get('.block.image');
+
+    cy.get('.imagecards-block .slider-arrow .right-arrow').click();
+    cy.get('.imagecards-block .slider-arrow .left-arrow').click();
   });
 });
