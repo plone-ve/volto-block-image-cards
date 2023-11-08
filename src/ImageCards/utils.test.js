643677ad09e351a97ceb9a3ebd9af3c6fa9fa07b
nileshgulia1 <nileshgulia@gmail.com>
2023-11-08 16:56:25 +0530

    tests: more specs

--- src/ImageCards/utils.test.js
+++ src/ImageCards/utils.test.js
@@ -1,11 +1,13 @@
 import { getImageScaleParams } from './utils';
+import { flattenToAppURL } from '@plone/volto/helpers';
 
 jest.mock('@plone/volto/helpers', () => ({
   flattenToAppURL: jest.fn((url) => url),
+  isInternalURL: jest.fn((url) => true),
 }));
 
 describe('getImageScaleParams', () => {
-  it('returns expected image scale URL when an image properties are passed', () => {
+  it('returns expected image scale URL obj when image_field and image_scales properties are passed', () => {
     const image = {
       '@id': 'http://localhost:3000/image',
       image_field: 'image',
@@ -27,21 +29,42 @@ describe('getImageScaleParams', () => {
       },
     };
 
-    const expectedUrl = '@@images/image-400.png';
-    expect(getImageScaleParams(image, 'preview')).toEqual(expectedUrl);
+    const expectedUrlObj = {
+      download: 'http://localhost:3000/image/@@images/image-400.png',
+      width: 400,
+      height: 400,
+    };
+    expect(getImageScaleParams(image, 'preview')).toEqual(expectedUrlObj);
   });
 
-  // it('returns expected image scale URL when API path is not present', () => {
-  //   const url = '/path/to/image';
-  //   const size = 'large';
-  //   const expectedUrl = '/path/to/image/@@images/image/large';
-  //   expect(getScaleUrl(url, size)).toEqual(expectedUrl);
-  // });
+  it('returns expected image scale URL obj when image properties are passed', () => {
+    const image = {
+      '@id': 'http://localhost:3000/image',
+      image: {
+        download: 'http://localhost:3000/image/@@images/image.png',
+        width: 400,
+        height: 400,
+        scales: {
+          preview: {
+            download: 'http://localhost:3000/image/@@images/image-400.png',
+            width: 400,
+            height: 400,
+          },
+        },
+      },
+    };
+    const expectedUrlObj = {
+      download: 'http://localhost:3000/image/@@images/image-400.png',
+      width: 400,
+      height: 400,
+    };
+    expect(getImageScaleParams(image, 'preview')).toEqual(expectedUrlObj);
+  });
 
-  // it('calls flattenToAppURL when API path is present', () => {
-  //   const url = '/api/path/to/image';
-  //   const size = 'large';
-  //   getScaleUrl(url, size);
-  //   expect(flattenToAppURL).toHaveBeenCalledWith('/path/to/image');
-  // });
+  it('calls flattenToAppURL when internalUrl', () => {
+    const url = 'http://localhost:3000/image';
+    const size = 'large';
+    getImageScaleParams(url, size);
+    expect(flattenToAppURL).toHaveBeenCalledWith('http://localhost:3000/image');
+  });
 });
