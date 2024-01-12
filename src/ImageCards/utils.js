b76940d332134616def28bd6c161b03c78c28d01
Miu Razvan <miu.razvan28@gmail.com>
2024-01-12 18:19:50 +0200

    fix cards

--- src/ImageCards/utils.js
+++ src/ImageCards/utils.js
@@ -1,7 +1,9 @@
 import config from '@plone/volto/registry';
 import { flattenToAppURL } from '@plone/volto/helpers';
 
-export const getScaleUrl = (url, size) =>
-  (url || '').includes(config.settings.apiPath)
+export const getScaleUrl = (url, size) => {
+  if (!url) return url;
+  return url.includes(config.settings.apiPath)
     ? `${flattenToAppURL(url.replace('/api', ''))}/@@images/image/${size}`
     : `${url.replace('/api', '')}/@@images/image/${size}`;
+};
